fb080e2ee5b4bd048c3fbec7fa8917a83fc1aa3c
D7703_30 <d7703_30@d7703-30ui-imac.local>
2019-03-28 16:57:05 +0900

    timer

--- alienimageview/ViewController.swift
+++ alienimageview/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageview: UIImageView!
+    @IBOutlet weak var countlabel: UILabel!
+    var count = 1
+    //timer 객체 선언
+    var mytimer = Timer();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imageview.image = UIImage(named: "frame\(count).png")
+        countlabel.text = String(count)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func playbtrPressed(_ sender: Any) {
+        if (!mytimer.isValid) {
+        mytimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func pausebtrPressed(_ sender: Any) {
+        mytimer.invalidate()
+    }
+    @IBAction func stopbtrPressed(_ sender: Any) {
+        mytimer.invalidate()
+    }
+    
+    //Timer 에서 호출되는 함수 정의
+    @objc func doAnimation() {
+        count += 1
+        if count == 0 {
+            count = 1
+        }
+        imageview.image = UIImage(named: "frame\(count).png")
+        countlabel.text = String(count)
+    }
+    
+    
 }
 
